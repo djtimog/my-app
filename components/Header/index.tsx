25b91698776a6fd9fc767f28248e393281adf142
djtimog <ogunleyetimilehin14@gmail.com>
2024-09-03 17:13:01 +0100

    worked on header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -12,7 +12,7 @@ export default function Header() {
         setOpen(!open);
     }
     return <>
-        <header className={`w-100 p-2vh p-7vw position-fixed`}>
+        <header className={`w-100 p-2vh p-7vw position-fixed ${styles.header}`}>
             <nav className={`w-100 d-flex justify-content-between`}>
                 <div>
                     <Link href="/">
@@ -23,12 +23,12 @@ export default function Header() {
                     <div className="d-md-none text-end">
                         <FontAwesomeIcon className={`${styles.hamburger}`} onClick={toggle} icon={open ? faTimes : faBars} />
                     </div>
-                    <ul className={`${open ? '' : 'd-none '}text-end d-md-flex align-items-center list-unstyled gap-20`}>
-                        <li><Link href={routes.support.contact}>Packages</Link></li>
-                        <li><Link href={routes.support.about}>Testimonial</Link></li>
-                        <li><Link href={routes.auth.signin}>Offers</Link></li>
-                        <li><Link className="btn btn-secondary" href={routes.auth.signup}>Sign Up</Link></li>
-                        <li><Link className="btn bg-transparent border-secondary" href={routes.auth.signin}>Login</Link></li>
+                    <ul className={`${open ? '' : 'd-none '} d-md-flex align-items-center list-unstyled gap-20`}>
+                        <li className='p-2 p-md-0 text-center'><Link href={routes.support.contact}>Packages</Link></li>
+                        <li className='p-2 p-md-0 text-center'><Link href={routes.support.about}>Testimonial</Link></li>
+                        <li className='p-2 p-md-0 text-center'><Link href={routes.auth.signin}>Offers</Link></li>
+                        <li className='p-2 p-md-0 text-center'><Link className="btn btn-secondary" href={routes.auth.signup}>Sign Up</Link></li>
+                        <li className='p-2 p-md-0 text-center'><Link className="btn bg-transparent border-secondary" href={routes.auth.signin}>Login</Link></li>
                     </ul>
                 </div>
             </nav>
