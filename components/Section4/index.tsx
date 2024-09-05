57bdb88398896bbbb14a048eb569bb96505b7d76
djtimog <ogunleyetimilehin14@gmail.com>
2024-09-05 22:35:21 +0100

    ...

--- components/Section4/index.tsx
+++ components/Section4/index.tsx
@@ -19,35 +19,35 @@ export default function Section4(){
     return(
         <>
             <section className={` w-100 p-4 p-sm-5 py-5`}>
-                <div className="top-avatar w-100 d-flex justify-content-between px-lg-5">
+                <div className={`${styles.topAvatar} w-100 d-flex justify-content-between px-lg-5`}>
                     <div>
                         <Image src={firstAvatar} alt={`avatar`} title={"I can take care of your pitch"} className="d-block d-lg-none" width={70} height={70}/>
-                        <Image src={lgFirstAvatar} alt={`avatar`} className="bgfirst d-none d-lg-block"/>
+                        <Image src={lgFirstAvatar} alt={`avatar`} className="bigfirst d-none d-lg-block" width={300} height={110}/>
                     </div>
                     <div>
                         <Image src={secondAvatar} alt={`avatar`} title={"I can help marketing strategy"} className="d-block d-lg-none" width={70} height={70}/>
-                        <Image src={lgSecondAvatar} alt={`avatar`} className="bigsecond d-none d-lg-block"/>
+                        <Image src={lgSecondAvatar} alt={`avatar`} className="bigsecond d-none d-lg-block" width={300} height={110}/>
                     </div>
                 </div>
-                <div className="z-index-1">
+                <div>
                     <div className="text-center m-1 m-sm-2 m-md-5 p-1 p-sm-2 p-md-5">
                         <h3 className="mb-4"><b>Doubting? Hear from others</b></h3>
                         <p className="mb-5">Do you require some help for your project: Conception workshop,<br/>prototyping, marketing strategy, landing page, UX/UI?</p>
                         <Link href={routes.auth.signup} className="btn btn-primary px-5 py-3"> Register Now</Link>
                     </div>
                 </div>
-                <div className="bottom-avatar w-100 d-flex justify-content-between">
+                <div className={`${styles.bottomAvatar} w-100 d-flex justify-content-between`}>
                     <div>
                         <Image src={thirdAvatar} alt={`avatar`} title={"I can design you website"} className="d-block d-lg-none" width={70} height={70}/>
-                        <Image src={lgThirdAvatar} alt={`avatar`} className="bigthird d-none d-lg-block"/>
+                        <Image src={lgThirdAvatar} alt={`avatar`} className="bigthird d-none d-lg-block" width={300} height={110}/>
                     </div>
                     <div>
                         <Image src={fourthAvatar} alt={`avatar`} title={"I will define the profile of your users"} className="d-block d-lg-none" width={70} height={70}/>
-                        <Image src={lgFourthAvatar} alt={`avatar`} className="bigfourth d-none d-lg-block"/>
+                        <Image src={lgFourthAvatar} alt={`avatar`} className="bigfourth d-none d-lg-block" width={300} height={110}/>
                     </div>
                     <div>
                         <Image src={fifthAvatar} alt={`avatar`} title={"I can prototype your app"} className="d-block d-lg-none" width={70} height={70}/>
-                        <Image src={lgFifthAvatar} alt={`avatar`} className="bigfifth d-none d-lg-block"/>
+                        <Image src={lgFifthAvatar} alt={`avatar`} className="bigfifth d-none d-lg-block" width={300} height={110}/>
                     </div>
                 </div>
             </section>
