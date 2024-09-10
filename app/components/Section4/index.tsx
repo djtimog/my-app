9ca37c46cd0b6f57dc2fe779c38a013e030998b0
djtimog <ogunleyetimilehin15@gmail.com>
2024-09-10 17:33:07 +0100

    remove small avatars and make soma animations for the lg avatars

--- app/components/Section4/index.tsx
+++ app/components/Section4/index.tsx
@@ -3,36 +3,36 @@ import routes from "@/routes";
 import Link from "next/link";
 import Image from "next/image";
 import firstAvatar from "@/img/avatar/img1.png";
-import lgFirstAvatar from "@/img/avatar/lg-avatar/img1.png";
 import secondAvatar from "@/img/avatar/img2.png";
-import lgSecondAvatar from "@/img/avatar/lg-avatar/img2.png";
 import thirdAvatar from "@/img/avatar/img3.png";
-import lgThirdAvatar from "@/img/avatar/lg-avatar/img3.png";
 import fourthAvatar from "@/img/avatar/img4.png";
-import lgFourthAvatar from "@/img/avatar/lg-avatar/img4.png";
 import fifthAvatar from "@/img/avatar/img5.png";
-import lgFifthAvatar from "@/img/avatar/lg-avatar/img5.png";
+import { useEffect, useState } from "react";
 
 export default function Section4() {
+    const [display, setDisplay] = useState(true);
+    useEffect(() => {
+        const interval = setInterval(() => setDisplay(!display), 5000);
+        return () => clearInterval(interval);
+    }, [display]);
     const topAvatars = [
-        {src: firstAvatar, alt: "first avatar", title: "I can take care of your pitch", width: 70, height: 70, largeSrc: lgFirstAvatar, largeWidth: 300, largeHeight: 110, largeClass: styles.lgfirst},
-        {src: secondAvatar, alt: "second avatar", title: "I can help marketing strategy", width: 70, height: 70, largeSrc: lgSecondAvatar, largeWidth: 300, largeHeight: 110, largeClass: styles.lgsecond},
+        {src: firstAvatar, alt: "first avatar", title: "I can take care of your pitch", className:`${styles.first} ${display === true?"":"d-none"} d-md-block`},
+        {src: secondAvatar, alt: "second avatar", title: "I can help marketing strategy", className: `${styles.second} ${display === true?"d-none":""} d-md-block`},
     ];
     
     const bottomAvatars = [
-        {src: thirdAvatar, alt: "third avatar", title: "I can design you website", width: 70, height: 70, largeSrc: lgThirdAvatar, largeWidth: 300, largeHeight: 110, largeClass: styles.lgthird},
-        {src: fourthAvatar, alt: "fourth avatar", title: "I will define the profile of your users", width: 70, height: 70, largeSrc: lgFourthAvatar, largeWidth: 300, largeHeight: 110, largeClass: styles.lgfourth},
-        {src: fifthAvatar, alt: "fifth avatar", title: "I can prototype your app", width: 70, height: 70, largeSrc: lgFifthAvatar, largeWidth: 300, largeHeight: 110, largeClass: styles.lgfifth},
+        {src: thirdAvatar, alt: "third avatar", title: "I can design you website", className:`${styles.third} ${display === true?"":"d-none"}`},
+        {src: fourthAvatar, alt: "fourth avatar", title: "I will define the profile of your users", className: `${styles.fourth} d-none d-sm-block`},
+        {src: fifthAvatar, alt: "fifth avatar", title: "I can prototype your app", className:`${styles.fifth} ${display === true?"d-none":""}`},
     ];
     
     return (
         <>
             <section className={`p-4 p-sm-5 py-5`}>
-                <div className={`${styles.topAvatar} d-flex justify-content-between px-lg-5`}>
+                <div className={`${styles.topAvatar} d-flex justify-content-center justify-content-md-between px-lg-5`}>
                     {topAvatars.map(avatar => (
                         <div key={avatar.alt}>
-                            <Image src={avatar.src} alt={avatar.alt} title={avatar.title} className="d-block d-lg-none" width={avatar.width} height={avatar.height}/>
-                            <Image src={avatar.largeSrc} alt={avatar.alt} title={avatar.title} className={`${avatar.largeClass} d-none d-lg-block`} width={avatar.largeWidth} height={avatar.largeHeight}/>
+                            <Image src={avatar.src} alt={avatar.alt} title={avatar.title} className={`${avatar.className} img-fluid d-lg-block`} />
                         </div>
                     ))}
                 </div>
@@ -43,11 +43,10 @@ export default function Section4() {
                         <Link href={routes.auth.signup} className="btn btn-primary px-5 py-3"> Register Now</Link>
                     </div>
                 </div>
-                <div className={`${styles.bottomAvatar} d-flex justify-content-between`}>
+                <div className={`${styles.bottomAvatar} d-flex justify-content-center justify-content-md-between px-lg-5`}>
                     {bottomAvatars.map(avatar => (
                         <div key={avatar.title}>
-                            <Image src={avatar.src} alt={avatar.alt} title={avatar.title} className="d-block d-lg-none" width={avatar.width} height={avatar.height}/>
-                            <Image src={avatar.largeSrc} alt={avatar.alt} title={avatar.title} className={`${avatar.largeClass} d-none d-lg-block`} width={avatar.largeWidth} height={avatar.largeHeight}/>
+                            <Image src={avatar.src} alt={avatar.alt} title={avatar.title} className={`${avatar.className} img-fluid d-lg-block`}/>
                         </div>
                     ))}
                 </div>
