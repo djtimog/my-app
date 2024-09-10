8b1e9a14f91709dc126a60626063a2d7f6b38b80
djtimog <ogunleyetimilehin15@gmail.com>
2024-09-10 14:27:34 +0100

    added more email validation

--- app/auth/components/Password/reset/index.tsx
+++ app/auth/components/Password/reset/index.tsx
@@ -10,7 +10,11 @@ const ResetModal = ({ onClick }:{onClick:() => void}) => {
     const validationSchema = Yup.object().shape({
         email: Yup.string()
         .email('Invalid Email address')
-        .required('Email address is required'),
+        .required('Email address is required')
+        .matches(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          'Email address is invalid'
+        ),
       });
     
       const formOptions = { resolver: yupResolver(validationSchema) };
